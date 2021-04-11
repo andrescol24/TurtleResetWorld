02ba207a57d0de4daecec0bbd5cd78a967e36960
andrescol24 <andresmil24@hotmail.com>
2021-04-11 18:12:10 -0500

    language configuration and i deleted the copy-paste command

--- src/main/java/co/andrescol/mc/plugin/turtleresetworld/command/TurtleResetWorldCommand.java
+++ src/main/java/co/andrescol/mc/plugin/turtleresetworld/command/TurtleResetWorldCommand.java
@@ -14,7 +14,6 @@ public class TurtleResetWorldCommand extends AMainCommand {
 		this.addSubCommand(new RegenSubCommand());
 		this.addSubCommand(new TpSubCommand());
 		this.addSubCommand(new CalcTimeRegenSubCommand());
-		this.addSubCommand(new CopyPasteSubCommand());
 	}
 
 	@Override
